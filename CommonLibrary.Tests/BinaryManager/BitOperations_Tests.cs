da384997ee5f10dec7dd75d494ec40ec69795647
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2026-09-04 21:11:47 +0300

    Removed tests

---
+++ CommonLibrary.Tests/BinaryManager/BitOperations_Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace CommonLibrary.Tests.BinaryManager
+{
+    using CommonLibrary.BinaryManager;
+
+    /// <summary>
+    ///  Provides test for BitOperations class functionality.
+    /// </summary>
+    [TestFixture]
+    public class BitOperations_Tests
+    {
+        /// <summary>
+        ///  Checks the IsActiveBit() function.
+        /// </summary>
+        [Test]
+        public void Test_IsActiveBitWorksFineWhenTrue()
+        {
+            int five = 0b00000101;
+            bool result = BitOperations.IsActiveBit(five, 0);
+            Assert.That(() => result, Is.True);
+        }
+    }
+}
